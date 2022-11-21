6676cb6e281bba7e5c34c77f8bfc6b771b8765ae
fasilmsageer <fasilmsageer@gmail.com>
2022-11-21 12:39:35 +0530

    secondcommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        char operator;
+        Double a, b, c;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose an option : ");
+        System.out.println("1 : find largest of 3 number");
+        System.out.println("2 : find smallest of 3 number");
+        System.out.println("3 : find prime or not");
+        System.out.println("4 : find even or not");
+        System.out.println("5 : find reverse of the number");
+        System.out.println("6 : exit");
+
+
+
     }
 }
